cc907a1d91ecbd7efbe10366f48012a6ef94fb4a
Pankaj Devrani <pandevra@publicisgroupe.net>
2024-01-25 11:39:38 +0530

    Adding 2nd DSA question

--- 229-Majority-Element II.go
+++ 229-Majority-Element II.go
@@ -1,11 +1,10 @@
-// Given an array nums of size n, return the majority element.
-// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+// Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.
 // Example 1:
 // Input: nums = [3,2,3]
-// Output: 3
+// Output: [3]
 // Example 2:
-// Input: nums = [2,2,1,1,1,2,2]
-// Output: 2
+// Input: nums = [1,2]
+// Output: [1,2]
 package main
 
 import "fmt"
