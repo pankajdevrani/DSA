5290190fa5de7ec5b8bbf310bf85c8e780660865
Pankaj Devrani <pandevra@publicisgroupe.net>
2024-10-23 21:19:27 +0530

    adding more questions

---
+++ Searching-Algorithm/linearsearch.py
@@ -0,0 +1,7 @@
+def linearSearch(array, value):
+    for i in range(len(array)):
+        if array[i] == value:
+            return 1
+    return -1
+        
+print(linearSearch([20,30,50,80,90], 90))
